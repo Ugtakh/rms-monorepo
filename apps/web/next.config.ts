fd23aa5ed7da0ca2f2d672332299c5003c56c412
Ugtakh <ugtakhbayars@gmail.com>
2026-04-07 13:07:57 +0800

    third app

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ["@rms/types", "@rms/constants"]
+  transpilePackages: ["@rms/shared"]
 };
 
 export default nextConfig;
